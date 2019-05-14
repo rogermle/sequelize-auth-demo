f38523a2e10633e992a33582dfc483315bb6783d
Roger Le <rogermle@gmail.com>
2019-05-14 13:13:50 -0400

    Created server and linked dependencies

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const passport   = require('passport');
+const session    = require('express-session');
+const bodyParser = require('body-parser');
+
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// For Passport
+ 
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+app.get('/', function(req, res) {
+ 
+    res.send('Welcome to Passport with Sequelize');
+ 
+});
+ 
+ 
+app.listen(PORT, function(err) {
+ 
+    if (!err)
+        console.log("Site is live");
+    else console.log(err)
+ 
+});
