65ddb9212aff157e8d1739a3ba794d5dc6d9ea3d
Roger Le <rogermle@gmail.com>
2019-05-14 14:36:45 -0400

    Created Signup, sign in, and dashboard

---
+++ routes/auth.js
@@ -0,0 +1,25 @@
+const authController = require('../controllers/authcontroller.js');
+ 
+module.exports = function(app, passport) {
+    app.get('/signup', authController.signup); 
+    app.get('/signin', authController.signin);
+    app.get('/dashboard',isLoggedIn, authController.dashboard);
+    app.post('/signin', passport.authenticate('local-signin', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/signin'
+    }));
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/signup'
+    }));
+    app.get('/logout',authController.logout);
+}
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()){
+        return next();
+    }
+    
+    res.redirect('/signin');
+ 
+}
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const env = require('dotenv').config();
 
 const passport   = require('passport');
 const session    = require('express-session');
 const bodyParser = require('body-parser');
+var exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 //For BodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,17 +21,27 @@ app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true}))
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 
+//Models
+const db = require("./models");
+
+//Sync Database
+db.sequelize.sync().then(function() {
+    console.log('Nice! Database looks fine')
+}).catch(function(err) {
+    console.log(err, "Something went wrong with the Database Update!")
+});
+
+const authRoute = require('./routes/auth.js')(app, passport);
+
+//load passport strategies
+require('./config/passport/passport.js')(passport, db.User);
+
 app.get('/', function(req, res) {
- 
-    res.send('Welcome to Passport with Sequelize');
- 
+    res.render('home');
 });
  
- 
 app.listen(PORT, function(err) {
- 
     if (!err)
         console.log("Site is live");
     else console.log(err)
- 
 });
